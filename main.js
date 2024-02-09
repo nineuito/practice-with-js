b09896f285512546200f04ec525c0f7514d0e6d9
Oleh Chernov <nineuito@mail.com>
2024-02-09 09:38:58 +0200

    practice

--- main.js
+++
@@ -1,9 +0,0 @@
-function calculateProfit(amount, percent, period) {
-  let currentAmount = amount;
-  
-  for (let i = 1; i <= period; i++) {
-    currentAmount += currentAmount * (percent / 100);
-  }
-
-  return currentAmount - amount;
-}
